343587e2902752f37fb60fe1de5a9088e4370cb5
Gerard <kanetpwnz@gmail.com>
2023-07-12 03:07:20 +0200

    solved exercise

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,1 +1,13 @@
-sub-strings project
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substring(string, dictionary)
+    sumapalabras = Hash.new
+    string.split(' ').each {|word|
+        dictionary.each {|palabra|
+          if palabra.downcase.index(word.downcase).is_a? Integer
+            sumapalabras.has_key?(word) ? sumapalabras[word] += 1 : sumapalabras[word] = 1
+          end
+        }
+    }
+    return sumapalabras
+end
